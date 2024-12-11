953b3aef6d50bf07019baf9ea08fbdaa30af4ce9
Vasant18 <vasantspujar018@gmail.com>
2024-12-12 01:38:46 +0530

    Refactor database client connection

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,8 @@
+import { config } from 'dotenv';
 import { Client } from 'pg';
-
+config();
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client({connectionString: process.env.DATABASE_URL,});
     await client.connect();
     return client;
 }
